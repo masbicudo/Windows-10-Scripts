1a19e7794d3cc51fc5cd02623eb84258d9bc69e6
Miguel Angelo <masbicudo@gmail.com>
2019-06-11 12:23:45 -0300

    set-where.cmd - support for default values via env vars

--- set-where.cmd
+++ set-where.cmd
@@ -22,11 +22,13 @@ ECHO.
 ECHO.%R%Command line:%N%
 ECHO.%W%%script_name% %C%varname%W% %C%object%W% [%C%parts%W%] [%C%options%W%]%N%
 ECHO.%C%varname%W%: name of the variable to receive the location of the desired object.
-ECHO.%C%object%W%:  file ^| file.ext ^| folder
+ECHO.%C%object%W%:  %Y%file%N% ^| %Y%file.ext%N% ^| %Y%folder%N%
 ECHO.         A file without an extension will be searched by adding each extension in the PATHEXT variable.
 ECHO.         A file with an extension will only be located with that specific extension.
+ECHO.             (i.e. %_Y%file.ext%N% will not match %_Y%file.ext.xyz%N%)
 ECHO.         A folder name can be located, if Everything tool 'es.exe' is installed.
-ECHO.%C%parts%W%:   %Y%[d][p][n][x]%N%
+ECHO.         Env vars %B%__SEARCH__%N% and %B%__EXT__%N% can be used to specify object name and extension.
+ECHO.%C%parts%W%:   %W%[%Y%d%W%][%Y%p%W%][%Y%n%W%][%Y%x%W%]%N%
 ECHO.         %Y%d%N% - drive letter followed by double collons ":", e.g.: %_y%C:%N%
 ECHO.         %Y%p%N% - path of file, starting but not ending with backslash "\", e.g.: %_y%\DirA\DirB%N%
 ECHO.             unless %Y%n%N% or %Y%x%N% are specified, in which case a backslash is used as connector.
@@ -37,19 +39,24 @@ ECHO.         Multiple can be specified, e.g.: %Y%dp%N%, %Y%nx%N%
 ECHO.         When these flags are defined, variables in the path will get expanded:
 ECHO.             %Y%%%%_y%USERPROFILE%Y%%%%_y%\AppData\Roaming%N% --^> %_y%%USERPROFILE%\AppData\Roaming%N%
 ECHO.         Leave it empty to use the value as specified by the source.
+ECHO.         Can be defined through environment variable %B%__DPNX__%N%.
 ECHO.%C%options%W%:
 ECHO.         %C%--filter%N% %Y%str%N%: any string to filter the result, useful to select among many possible resulting locations.
-ECHO.         %C%--verbose%N% %Y%[0 ^| 1 ^| 2 ^| 3]%N%:
-ECHO.             %Y%0%N% - no output
-ECHO.             %Y%1%N% - result output
-ECHO.             %Y%2%N% - debug output
+ECHO.             Env var %B%__FILTER__%N% can be used to specify the filter.
+ECHO.         %C%--verbose%N% %W%[ %Y%0 %W%^|%Y% 1 %W%^|%Y% 2 %W%^|%Y% 3%W% ]%N%:
+ECHO.             %Y%0%N% - no output (default when %B%__VERBOSE__%N% is %Y%F%N%)
+ECHO.             %Y%1%N% - result output (default if %C%--verbose%N% is not present nor %B%__VERBOSE__%N%)
+ECHO.             %Y%2%N% - debug output (default when using %C%--verbose%N% alone, or %B%__VERBOSE__%N% is %Y%T%N%)
 ECHO.             %Y%3%N% - params output
 ECHO.             Each verbose level also prints the previous levels outputs.
-ECHO.         %C%--colors%N%: output with colors
-ECHO.         %C%--clear%N%: allow to undefine variable, if nothing is found.
-ECHO.         %C%--keep%N%: does not allow to redefine the variable, if it is already defined.
+ECHO.             Env var %B%__VERBOSE__%N% can be used to specify the verbosity level, in addition to %W%[%Y% T %W%^|%Y% F %W%]%N%
+ECHO.         %C%--colors%N%: output with colors; Env var: %B%__COLORS__%N% = %W%[%Y% T %W%^|%Y% F %W%]%Y%%N%
+ECHO.         %C%--no-colors%N%: output without colors, used to override env var %B%__COLORS__%N%
+ECHO.         %C%--clear%N%: allow to undefine variable, if nothing is found; Env var: %B%__CLEAR__%N% = %W%[%Y% T %W%^|%Y% F %W%]%Y%%N%
+ECHO.         %C%--keep%N%: does not allow to redefine the variable, if it is already defined; Env var: %B%__KEEP__%N% = %W%[%Y% T %W%^|%Y% F %W%]%Y%%N%
 ECHO.         %C%--test%N%: does not set variables, only displays output and returns ERRORLEVEL.
-ECHO.         %C%--unit-tests%N%: runs unit tests.
+ECHO.             Env var with oposite meaning: %B%__SET__%N% = %W%[%Y% T %W%^|%Y% F %W%]%Y%%N%
+ECHO.         %C%--unit-tests%N%: runs unit tests; Env var: %B%__UNIT_TESTS__%N% = %W%[%Y% T %W%^|%Y% F %W%]%Y%%N%
 ECHO.         %C%--debug-lines%N%: debug important lines
 ECHO.
 ECHO.%R%ERRORLEVEL:%N%
@@ -117,19 +124,26 @@ FOR %%C IN (_k;_r;_g;_y;_b;_m;_c;_w;K;R;G;Y;B;M;C;W;N) DO SET %%C=
 
 :: Initializing parameter variables
 SET _num_vars=__VERBOSE__;__ORD__
-SET _bool_vars=__CLEAR__;__KEEP__;__HELP__;__SET__;__UNIT_TESTS__
+SET _bool_vars=__CLEAR__;__KEEP__;__HELP__;__SET__;__UNIT_TESTS__;__COLORS__
 SET _str_vars=__VAR__;__SEARCH__;__EXT__;__DPNX__;__FILTER__
 SET _char_vars=__d;__p;__n;__x
 
-FOR %%C IN (%_num_vars%;%_str_vars%;%_bool_vars%;%_char_vars%) DO SET %%C=
+FOR %%C IN (__ORD__;__VAR__;%_char_vars%) DO SET %%C=
+
+SET _verb_n=1
+SET _verb_t=2
+SET _verb_f=0
 
 SET __ORD__=1
-SET __VERBOSE__=1
-SET __CLEAR__=F
-SET __KEEP__=F
-SET __HELP__=F
-SET __SET__=T
-SET __UNIT_TESTS__=F
+IF NOT DEFINED __VERBOSE__    SET __VERBOSE__=%_verb_n%
+IF /I "%__VERBOSE__%"=="T"    SET __VERBOSE__=%_verb_t%
+IF /I "%__VERBOSE__%"=="F"    SET __VERBOSE__=%_verb_f%
+IF NOT DEFINED __CLEAR__      SET __CLEAR__=F
+IF NOT DEFINED __KEEP__       SET __KEEP__=F
+IF NOT DEFINED __HELP__       SET __HELP__=F
+IF NOT DEFINED __SET__        SET __SET__=T
+IF NOT DEFINED __UNIT_TESTS__ SET __UNIT_TESTS__=F
+IF NOT DEFINED __COLORS__     SET __COLORS__=F
 
 :: Parsing parameters
 :BEGIN_LOOP_PARAMS
@@ -138,23 +152,24 @@ IF "%~1"=="" IF [%1]==[] GOTO :END_LOOP_PARAMS
 %DL% SET __ARG__=%1
 SET __ARG__=%1
 IF "%~1"=="--verbose" (
-  %DL% SET __VERBOSE__=2
-  SET __VERBOSE__=2
+  %DL% SET __VERBOSE__=%_verb_t%
+  SET __VERBOSE__=%_verb_t%
   SHIFT
   IF "%~2"=="0" ( SET "__VERBOSE__=0" & SHIFT )
   IF "%~2"=="1" ( SET "__VERBOSE__=1" & SHIFT )
   IF "%~2"=="2" ( SET "__VERBOSE__=2" & SHIFT )
   IF "%~2"=="3" ( SET "__VERBOSE__=3" & SHIFT )
 ) ELSE IF "%~1"=="--clear"      ( SET "__CLEAR__=T"      & SHIFT
-) ELSE IF "%~1"=="--debug-lines" (                          SHIFT
+) ELSE IF "%~1"=="--debug-lines" (                         SHIFT
 ) ELSE IF "%~1"=="--keep"       ( SET "__KEEP__=T"       & SHIFT
 ) ELSE IF "%~1"=="--test"       ( SET "__SET__=F"        & SHIFT
 ) ELSE IF "%~1"=="--unit-tests" ( SET "__UNIT_TESTS__=T" & SHIFT
 ) ELSE IF "%~1"=="--help"       ( SET "__HELP__=T"       & SHIFT
 ) ELSE IF "%~1"=="-help"        ( SET "__HELP__=T"       & SHIFT
 ) ELSE IF "%~1"=="/?"           ( SET "__HELP__=T"       & SHIFT
-) ELSE IF "%~1"=="--filter"     ( SET __FILTER__="%~2"   & SHIFT & SHIFT
-) ELSE IF "%~1"=="--colors"     ( call :SUB_SET_COLORS   & SHIFT
+) ELSE IF "%~1"=="--filter"     ( SET __FILTER__="%~2"&    SHIFT & SHIFT
+) ELSE IF "%~1"=="--no-colors"  ( SET __COLORS__=F&        SHIFT
+) ELSE IF "%~1"=="--colors"     ( SET __COLORS__=T&        SHIFT
 ) ELSE IF "%__ORD__%"=="1" (
   %DL% SET "__VAR__=%~1"
   SET "__VAR__=%~1"
@@ -175,6 +190,9 @@ IF "%~1"=="--verbose" (
 %DL% GOTO :BEGIN_LOOP_PARAMS
 GOTO :BEGIN_LOOP_PARAMS
 :END_LOOP_PARAMS
+
+:: Colors
+IF /I "%__COLORS__%"=="T" call :SUB_SET_COLORS
 
 :: DPNX parameters (extract path components)
 %DL% SET __DPNX__=_%__DPNX__%
@@ -205,8 +223,8 @@ ENDLOCAL
 :: global filters using findstr
 IF DEFINED __FILTER__ SET __FILTER__=^^^| findstr /I %__FILTER__:\=\\%
 
-IF "%__UNIT_TESTS__%"=="T" call :SUB_TESTS
-IF "%__HELP__%"=="T" call :SUB_HELP
+IF  /I "%__UNIT_TESTS__%"=="T" call :SUB_TESTS
+IF  /I "%__HELP__%"=="T" call :SUB_HELP
 IF %__ORD__% LEQ 2 GOTO :EOF
 
 :IF_Begin
@@ -262,6 +280,7 @@ IF %__ORD__% LEQ 2 GOTO :EOF
   SET "__Search_Findstr=%__Search_Findstr:\=\\%"
   SET __ES_Found=0
   IF "%__EXT__%"=="" (
+    :: File search with each PATHEXT extension
     FOR %%e IN (%PATHEXT%) DO (
       IF %__VERBOSE__% GEQ 2 ECHO.%C% es.exe "%__SEARCH__%%%e" "*%%e" ^| findstr /I /E "\\%__SEARCH__%%%e" 2^>NUL %__FILTER__%%N%
       FOR /F "tokens=* usebackq" %%a IN (` es.exe "%__SEARCH__%%%e" "*%%e" ^| findstr /I /E "\\%__SEARCH__%%%e" 2^>NUL %__FILTER__% `) DO (
@@ -271,6 +290,7 @@ IF %__ORD__% LEQ 2 GOTO :EOF
         GOTO Exit_ES_For
       )
     )
+    :: File search without extension
     IF %__VERBOSE__% GEQ 2 ECHO.%C% es.exe "%__SEARCH__%" ^| findstr /I /E "%__Search_Findstr%" 2^>NUL %__FILTER__%%N%
     FOR /F "tokens=* usebackq" %%a IN (` es.exe "%__SEARCH__%" ^| findstr /I /E "%__Search_Findstr%" 2^>NUL %__FILTER__% `) DO (
       SET "__VALUE__=%%a"
@@ -279,6 +299,7 @@ IF %__ORD__% LEQ 2 GOTO :EOF
       GOTO Exit_ES_For
     )
   ) ELSE (
+    :: File search with extension
     IF %__VERBOSE__% GEQ 2 ECHO.%C% es.exe "%__SEARCH__%" "*%__EXT__%" ^| findstr /I /E "%__Search_Findstr%" 2^>NUL %__FILTER__%%N%
     FOR /F "tokens=* usebackq" %%a IN (` es.exe "%__SEARCH__%" "*%__EXT__%" ^| findstr /I /E "%__Search_Findstr%" 2^>NUL %__FILTER__% `) DO (
       SET "__VALUE__=%%a"
@@ -301,7 +322,7 @@ IF %__ORD__% LEQ 2 GOTO :EOF
   %DL% ECHO."%__VALUE__%"
   IF DEFINED __DPNX__ (
     FOR /F "tokens=* usebackq delims=" %%i IN (`ECHO."%__VALUE__%"`) DO (
-	    echo.%%~%__DPNX__%i
+	    %DL% ECHO.DPNX: %Y%%%~%__DPNX__%i%N%
       call SET "__VALUE__=%%~%__DPNX__%i"
     )
   )
@@ -315,13 +336,13 @@ IF %__ORD__% LEQ 2 GOTO :EOF
 
   %DL% IF %__VERBOSE__% GEQ 1 ECHO.%W%%__VAR__%%K%=%Y%%__VALUE__%%N%
   IF %__VERBOSE__% GEQ 1 ECHO.%W%%__VAR__%%K%=%Y%%__VALUE__%%N%
-  IF DEFINED %__VAR__% IF "%__KEEP__%"=="T" SET __SET__=F
-  ENDLOCAL & IF "%__SET__%"=="T" SET "%__VAR__%=%__VALUE__%"
+  IF DEFINED %__VAR__% IF /I "%__KEEP__%"=="T" SET __SET__=F
+  ENDLOCAL & IF /I "%__SET__%"=="T" SET "%__VAR__%=%__VALUE__%"
 
   EXIT /B 0
 
 :Raise_Error
   IF %__VERBOSE__% GEQ 1 ECHO.%R%File not found%N%
-  ENDLOCAL & IF "%__SET__%"=="T" IF "%__CLEAR__%"=="T" SET "%__VAR__%="
+  ENDLOCAL & IF /I "%__SET__%"=="T" IF /I "%__CLEAR__%"=="T" SET "%__VAR__%="
   :: calling `where` with invalid argument to set errorlevel and return value
   where / 2>NUL 1>NUL
