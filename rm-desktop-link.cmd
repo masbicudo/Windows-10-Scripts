0c31e650ee6bb9a1ed27d3132e03e416edc13395
Miguel Angelo <masbicudo@gmail.com>
2019-06-15 16:42:06 -0300

    Remove shortcuts from desktop now detects *.url files too

--- rm-desktop-link.cmd
+++ rm-desktop-link.cmd
@@ -1,8 +1,17 @@
 @echo off
 call require places.cmd
 
+:: TODO: set-out requires the compilation of `eat.c`
+
 setlocal
 call set-out __to_move__ es %1 *.lnk -path "%where_desktop%"
 if defined __to_move__ call MOVE "%__to_move__%" "%where_shortcuts%\"
+
+call set-out __to_move__ es %1 *.url -path "%where_desktop%"
+if defined __to_move__ call MOVE "%__to_move__%" "%where_shortcuts%\"
+
 call set-out __to_move__ es %1 *.lnk -path "%where_public_desktop%"
+if defined __to_move__ call MOVE "%__to_move__%" "%where_shortcuts%\"
+
+call set-out __to_move__ es %1 *.url -path "%where_public_desktop%"
 if defined __to_move__ call MOVE "%__to_move__%" "%where_shortcuts%\"
