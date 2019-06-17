5117f824d89b1f3620b8f23792bc229366220725
Miguel Angelo <masbicudo@gmail.com>
2019-06-17 15:37:25 -0300

    bug fix: set-out script var name with double-quotes

--- lib/setters/set-out.cmd
+++ lib/setters/set-out.cmd
@@ -6,12 +6,16 @@ FOR /f "usebackq tokens=*" %%a IN (` eat %* `) DO SET TEMP_EAT=%%a
 FOR /f "usebackq tokens=*" %%a IN (` %TEMP_EAT% ^|^| SET _DIE=!errorlevel!`) DO ENDLOCAL & SET "__VALUE=%%a" & goto :next
 :next
 
-::echo._DIE=%_DIE%
-::echo.__VALUE=%__VALUE%
+::SET DL=ECHO
+SET DL=REM
+
+%DL% varname=%1
+%DL% _DIE=%_DIE%
+%DL% __VALUE=%__VALUE%
 if defined _DIE %comspec% /c exit %_DIE% & goto :dieerrlvl
 if not defined __VALUE %comspec% /c exit 1 & goto :dieerrlvl
 
-ENDLOCAL & SET "%1=%__VALUE%"
+ENDLOCAL & SET "%~1=%__VALUE%"
 %comspec% /c exit 0 & goto :dieerrlvl
 
 :: FOOTER
