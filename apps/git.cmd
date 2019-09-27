82767a5134a701c8d04071e3bb04284141676c8f
Miguel Angelo <masbicudo@gmail.com>
2019-09-27 15:44:48 -0300

    More apps: pandoc; changed git: create shortcut "gitbash.lnk"

--- apps/git.cmd
+++ apps/git.cmd
@@ -1,2 +1,8 @@
 call require apps\choco.cmd
 choco install -y git --params "/GitAndUnixToolsOnPath"
+
+call set-where where_git git
+call set-where where_gitbash bash --filter git
+
+call shortcut-create.cmd "%where_shortcuts%\gitbash.lnk" "%where_gitbash%"
+call shortcut-create.cmd "%where_shortcuts%\gitbasha.lnk" "%where_gitbash%" -ra
--- apps/gitextensions.cmd
+++ apps/gitextensions.cmd
@@ -1,7 +1,10 @@
 call require apps\choco.cmd
+
 choco upgrade -y gitextensions
+
 call set-where where_gitex gitex.exe
 call set-where where_gitextensions GitExtensions.exe
+
 shortcut-create "%where_shortcuts%\gitex.lnk" "%where_gitex%" -i "%where_gitextensions%,0"
 call MOVE "%where_public_desktop%\Git Extensions.lnk" "%where_shortcuts%\"
 call rm-desktop-link "Git Extensions.lnk"
