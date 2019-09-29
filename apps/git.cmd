36896dc785a259114825df30dd6415dabd7e6fa3
Miguel Angelo <masbicudo@gmail.com>
2019-09-29 00:53:17 -0300

    Updated gitbash.lnk to start from current dir

--- apps/git.cmd
+++ apps/git.cmd
@@ -4,5 +4,5 @@ choco install -y git --params "/GitAndUnixToolsOnPath"
 call set-where where_git git
 call set-where where_gitbash bash --filter git
 
-shortcut-create "%where_shortcuts%\gitbash.lnk" "%where_gitbash%"
-shortcut-create "%where_shortcuts%\gitbasha.lnk" "%where_gitbash%" -ra
+shortcut-create "%where_shortcuts%\gitbash.lnk" "%where_gitbash%" -wd "."
+shortcut-create "%where_shortcuts%\gitbasha.lnk" "%where_gitbash%" -wd "." -ra
