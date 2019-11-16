99c96261855c61adb276f0216de6ba130654c680
Miguel Angelo <masbicudo@gmail.com>
2019-11-16 18:47:54 -0300

    dbms-tools.cmd - more pgAdmin shortcuts

--- apps/dbms-tools.cmd
+++ apps/dbms-tools.cmd
@@ -1,7 +1,7 @@
 call require apps\choco.cmd
 
 :: postgreSQL
-choco upgrade -y postgresql --params "/Password:'%__PWD__%'"
+    choco upgrade -y postgresql
 
     :: replace configuration to trust any localhost connection
     :: effectively making password not required for any user
@@ -9,14 +9,17 @@ choco upgrade -y postgresql --params "/Password:'%__PWD__%'"
     copy "%~dp0\..\res\pgsql\pg_hba.conf" "%where_pghba%" /Y
 
 :: pgAdmin4
-choco upgrade -y pgadmin4
-set-where "where_pgadmin4" "pgadmin4.exe"
-shortcut-create "%where_shortcuts%\pg.lnk" "%where_pgadmin4%" -ra
+    choco upgrade -y pgadmin4
+    set-where "where_pgadmin4" "pgadmin4.exe"
+    shortcut-create "%where_shortcuts%\pg.lnk" "%where_pgadmin4%" -ra
+    shortcut-create "%where_shortcuts%\pgadmin.lnk" "%where_pgadmin4%" -ra
+    shortcut-create "%where_shortcuts%\pgadmin4.lnk" "%where_pgadmin4%" -ra
 
 :: DBeaver
-choco upgrade -y dbeaver
-set-where "where_dbeaver" "dbeaver.exe"
-shortcut-create "%where_shortcuts%\db.lnk" "%where_dbeaver%"
+    choco upgrade -y dbeaver
+    set-where "where_dbeaver" "dbeaver.exe"
+    shortcut-create "%where_shortcuts%\db.lnk" "%where_dbeaver%"
+    shortcut-create "%where_shortcuts%\dbeaver.lnk" "%where_dbeaver%"
 
 :: SQLite
-choco upgrade -y sqlite
+    choco upgrade -y sqlite
