c884bf613f4dc1a75ce5db2236cd489ff37102fc
Miguel Angelo <masbicudo@gmail.com>
2019-11-29 00:28:04 -0300

    Update app: WSL Arch + Ubunut

--- apps/wsl.cmd
+++ apps/wsl.cmd
@@ -8,14 +8,12 @@ poshca "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Sub
 ::choco upgrade -y wsl-archlinux
 
 :: http://www.akitaonrails.com/2018/04/29/running-arch-linux-over-windows-10
-
-if not exist "%SystemDrive%\Arch" (
-    if not exist "%where_downloads%\Arch.zip" (
-        curl -O "%where_downloads%\Arch.zip" https://github.com/yuk7/ArchWSL/releases/download/19.3.19.1/Arch.zip
-    )
-    mkdir "%SystemDrive%\Arch"
-    7z.exe e "%where_downloads%\Arch.zip" -o"%SystemDrive%\Arch"
-)
+curl -O "%where_downloads%\Arch.zip" https://github.com/yuk7/ArchWSL/releases/download/19.11.16.0/Arch.zip
+mkdir "%SystemDrive%\Arch"
+7z.exe e "%where_downloads%\Arch.zip" -o"%SystemDrive%\Arch"
 
 shortcut-create "%where_shortcuts%\arch.lnk" "%SystemDrive%\Arch\Arch.exe"
 shortcut-create "%where_shortcuts%\archa.lnk" "%SystemDrive%\Arch\Arch.exe" -ra
+
+
+choco install wsl-ubuntu-1804
