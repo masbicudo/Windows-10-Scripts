6523acd7f12a83bfa9c9cb57b7b4d018e41f0f07
Miguel Angelo <masbicudo@gmail.com>
2020-02-17 12:22:02 -0300

    rm-rm-desktop-link - remove links only if directly under desktop folder + bug-fix: only %1 was being passed to search

--- lib/rm-desktop-link.cmd
+++ lib/rm-desktop-link.cmd
@@ -1,17 +1,18 @@
 @echo off
 call require places.cmd
 
-:: TODO: set-out requires the compilation of `eat.c`
+:: TODO: set-out requires the compilation of `eat.c`?
+::       it seems to be working well with `eat.cmd`
 
 setlocal
-call set-out __to_move__ es %1 *.lnk -path "%where_desktop%"
+call set-out __to_move__ es %* *.lnk -parent "%where_desktop%"
 if defined __to_move__ call MOVE "%__to_move__%" "%where_shortcuts%\"
 
-call set-out __to_move__ es %1 *.url -path "%where_desktop%"
+call set-out __to_move__ es %* *.url -parent "%where_desktop%"
 if defined __to_move__ call MOVE "%__to_move__%" "%where_shortcuts%\"
 
-call set-out __to_move__ es %1 *.lnk -path "%where_public_desktop%"
+call set-out __to_move__ es %* *.lnk -parent "%where_public_desktop%"
 if defined __to_move__ call MOVE "%__to_move__%" "%where_shortcuts%\"
 
-call set-out __to_move__ es %1 *.url -path "%where_public_desktop%"
+call set-out __to_move__ es %* *.url -parent "%where_public_desktop%"
 if defined __to_move__ call MOVE "%__to_move__%" "%where_shortcuts%\"
