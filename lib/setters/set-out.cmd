b59097c3c800106442443b1dfd3a69f063a4f5fd
Miguel Angelo <masbicudo@gmail.com>
2020-02-17 16:50:59 -0300

    set-out - returning the command errorlevel

--- lib/setters/set-out.cmd
+++ lib/setters/set-out.cmd
@@ -2,43 +2,61 @@
 
 if "%1"==":some" if "%2"=="" goto :some
 
-SETLOCAL DisableDelayedExpansion
+SETLOCAL EnableDelayedExpansion
 
+:: Indentation of the outputs
 IF defined __IND ( SET "ECHO=ECHO.%__IND%" ) ELSE ( SET "ECHO=ECHO." )
-SET "__VAR__=%~1"
 
-:: Colors
-IF /I "%__COLORS__%"=="T" call :SUB_SET_COLORS
-
-FOR /f "usebackq tokens=*" %%a IN (` eat %* `) DO SET TEMP_EAT=%%a
-SET TEMP_EAT=%TEMP_EAT:^=^^%
-rem %ECHO%TEMP_EAT=%TEMP_EAT%
-FOR /f "usebackq tokens=*" %%a IN (` CALL "%~dpnx0" :some `) DO SET "__VALUE__=%%a" & goto :next
-:next
-
-IF "%__VALUE__:~0,5%"=="_DIE=" (
-    SET %__VALUE__%
-    SET __VALUE__=
-)
-
+:: Debug messages command (ECHO shows messages, REM hides messages)
 ::SET DL=%ECHO%
 SET DL=REM
 
-%DL% varname=%1
-%DL% _DIE=%_DIE%
-%DL% __VALUE__=%__VALUE__%
-%ECHO%%W%%__VAR__%%K%=%Y%%__VALUE__%%N%
-if defined _DIE %comspec% /c exit %_DIE% & goto :dieerrlvl
-if not defined __VALUE__ %comspec% /c exit 1 & goto :dieerrlvl
+:: Colors
+IF /I "%__COLORS__%"=="T" call :Sub_Set_Colors
 
-ENDLOCAL & SET "%__VAR__%=%__VALUE__%"
-%comspec% /c exit 0 & goto :dieerrlvl
+:: the target variable name will be stored in __VAR__
+SET "__VAR__=%~1"
+
+FOR /f "usebackq tokens=*" %%a IN (` eat %* `) DO SET TEMP_EAT=%%a
+SET TEMP_EAT=%TEMP_EAT:^=^^%
+%DL% TEMP_EAT=%TEMP_EAT%
+
+SET "__NEXT__="
+SET "__VALUE__="
+
+FOR /f "usebackq tokens=*" %%a IN (` CALL "%~dpnx0" :some `) DO (
+  IF not defined __VALUE__ IF defined __NEXT__ SET "__VALUE__=!__NEXT__!"
+  SET "__NEXT__=%%a"
+)
+
+%DL% varname=%1
+%DL% __VALUE__=%__VALUE__%
+%DL% %__NEXT__%
+
+:: Last line contains _DIE=number
+SET %__NEXT__%
+
+:: TODO: option to undefine the variable if __VALUE__ is empty
+if defined __VALUE__ (
+  goto :set_value
+) else (
+  goto :unset_value
+)
+
+:set_value
+  %ECHO%%W%%__VAR__%%K%=%Y%%__VALUE__%%N%
+  ENDLOCAL & SET "%__VAR__%=%__VALUE__%" & %comspec% /c exit %_DIE%
+  goto :dieerrlvl
+:unset_value
+  %ECHO%%W%%__VAR__%%K%=%N%
+  ENDLOCAL & SET "%__VAR__%=" & %comspec% /c exit %_DIE%
+  goto :dieerrlvl
 
 goto :EOF
 :some
-
-%TEMP_EAT%
-ECHO _DIE=%errorlevel%
+  %TEMP_EAT% 2>&1
+  ECHO _DIE=%errorlevel%
+goto :EOF
 
 ::
 :: Set_Colors
