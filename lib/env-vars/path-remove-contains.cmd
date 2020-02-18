e1b8dd476a1bbe475efaa6a794a5ab01218b3753
Miguel Angelo <masbicudo@gmail.com>
2020-02-18 14:36:36 -0300

    path-remove-contains.cmd - protect machine level system entries in the path variable

--- lib/env-vars/path-remove-contains.cmd
+++ lib/env-vars/path-remove-contains.cmd
@@ -24,13 +24,17 @@
 :Header
   @echo off
   setlocal EnableDelayedExpansion
-  SET R=[91m&SET G=[92m&SET Y=[93m&SET B=[94m&SET M=[95m&SET C=[96m&SET W=[97m&SET N=[0m
+  SET LK=[90m&SET R=[91m&SET G=[92m&SET Y=[93m&SET B=[94m&SET M=[95m&SET C=[96m&SET W=[97m&SET N=[0m
+  SET DK=[30m&SET DR=[31m&SET DG=[32m&SET DY=[33m&SET DB=[34m&SET DM=[35m&SET DC=[36m&SET DW=[37m
+
   SET "arg_1=%1"
   SET "arg_1=%arg_1:"=%"
+  SET __HELP=
   if "%arg_1%"=="" SET __HELP=1
+  if "%arg_1%"=="?" SET __HELP=1
   if "%arg_1%"=="/?" SET __HELP=1
-  if "%arg_1%"=="-h" SET __HELP=1
-  if "%arg_1%"=="--help" SET __HELP=1
+  if /I "%arg_1%"=="-h" SET __HELP=1
+  if /I "%arg_1%"=="--help" SET __HELP=1
   if not defined __HELP goto :no_help
   set text_color=%W%
   for /f "delims=" %%a in (%~dpnx0) do (
@@ -70,35 +74,70 @@
   :no_help
 
 :Main
+  SET __TO_REMOVE=
+  :parse_args
+  SET "arg_1=%1"
+  if not defined arg_1 goto :end_args
+  SET "arg_1=%arg_1:"=%"
+  if "%arg_1%"=="" goto :end_args
+  if /I "%arg_1%"=="-s" SET "__SIMULATE=1" & goto :next_arg
+  if /I "%arg_1%"=="--simulate" SET "__SIMULATE=1" & goto :next_arg
+  if defined __TO_REMOVE SET "__TO_REMOVE=%__TO_REMOVE%;%1"
+  if not defined __TO_REMOVE SET "__TO_REMOVE=%1"
+  :next_arg
+  shift
+  goto :parse_args
+  :end_args
+
   SET __VAR__=Path
-  SET "__TO_REMOVE=%*"
-  if "%arg_1%"=="-s" SET __SIMULATE=1
-  if "%arg_1%"=="--simulate" SET __SIMULATE=1
-  IF defined __SIMULATE FOR /f "tokens=1*delims= " %%a IN ("%*") DO set "__TO_REMOVE=%%b"
-  ::echo __SIMULATE=%__SIMULATE%
-  ::echo __TO_REMOVE=%__TO_REMOVE%
+  REM echo __SIMULATE=%__SIMULATE%
+  REM echo __TO_REMOVE=%__TO_REMOVE%
+  REM goto :eof
   call :normalize __TO_REMOVE
 
-  SET "__src_name=local %__VAR__%"
+  SET "__type=local"
   CALL SET "__COPY=%%%__VAR__%%%"
   call :normalize __COPY
   call :remove_from_list __COPY
 
-  SET "__src_name=machine %__VAR__%"
+  SET "__type=machine"
   SET "__KEY=HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
   call :read_reg __REG_SYS_VAR %__VAR__%
+  REM echo __REG_SYS_VAR=%__REG_SYS_VAR%
   call :normalize __REG_SYS_VAR
   call :remove_from_list __REG_SYS_VAR
-  call :write_reg_path __REG_SYS_VAR
 
-  SET "__src_name=user %__VAR__%"
+  SET "__type=user"
   SET "__KEY=HKCU\Environment"
   call :read_reg __REG_USER_VAR %__VAR__%
   call :normalize __REG_USER_VAR
   call :remove_from_list __REG_USER_VAR
+
+  :: keep writes at the end, to prevent corruption due to batch script failure
+  :: ensure the following were not removed by accident
+  :: - %SystemRoot%\system32
+  :: - %SystemRoot%
+  :: - %SystemRoot%\System32\Wbem
+  :: - %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\
+  :: - %SYSTEMROOT%\System32\OpenSSH\
+  set __write=1
+  if not defined __REG_SYS_VAR set __write=0
+  set "__test=;%__REG_SYS_VAR:"=%;"
+  if /I "!__test:;%%SystemRoot%%\system32;=;!"=="%__test%" set __write=0
+  if /I "!__test:;%%SystemRoot%%;=;!"=="%__test%" set __write=0
+  if /I "!__test:;%%SystemRoot%%\System32\Wbem;=;!"=="%__test%" set __write=0
+  if /I "!__test:;%%SYSTEMROOT%%\System32\WindowsPowerShell\v1.0\;=;!"=="%__test%" set __write=0
+  if /I "!__test:;%%SYSTEMROOT%%\System32\OpenSSH\;=;!"=="%__test%" set __write=0
+  if "%__write%"=="1" (
+    SET "__KEY=HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+    call :write_reg_path __REG_SYS_VAR
+  ) else (
+    echo.%R%Error: cannot remove default system entries%N%
+  )
+  SET "__KEY=HKCU\Environment"
   call :write_reg_path __REG_USER_VAR
 
-  EndLocal & IF not defined __SIMULATE SET "%__VAR__%=%__COPY%"
+  EndLocal & IF not "%__SIMULATE%"=="1" SET "%__VAR__%=%__COPY%"
 
   goto :EOF
   :read_reg
@@ -142,21 +181,34 @@
   goto :EOF
   :append_to_result
     SET "_TEMP=%_INPUT%"
-    if defined __SIMULATE ( set "__msg=would remove" ) else set "__msg=removed"
-        ::echo __TO_REMOVE=%__TO_REMOVE%
+    if defined __SIMULATE ( set "__msg=%LK%would remove" ) else set "__msg=%DR%removed"
     for %%b in (%__TO_REMOVE%) do (
       if %%b neq "" (
         SET "_TEMP=!_TEMP:%%~b=%Y%%%~b%W%!"
       )
     )
-    IF "%_INPUT%"=="%_TEMP%" (
+    :: protecting machine level entries defined by the system
+    set __sysentry=0
+    if /I "%__type%"=="machine" (
+      if /I "%_INPUT%"=="%%SystemRoot%%\system32" set __sysentry=1
+      if /I "%_INPUT%"=="%%SystemRoot%%" set __sysentry=1
+      if /I "%_INPUT%"=="%%SystemRoot%%\System32\Wbem" set __sysentry=1
+      if /I "%_INPUT%"=="%%SYSTEMROOT%%\System32\WindowsPowerShell\v1.0\" set __sysentry=1
+      if /I "%_INPUT%"=="%%SYSTEMROOT%%\System32\OpenSSH\" set __sysentry=1
+    )
+    set __add=0
+    set __selected=1
+    if "%__sysentry%"=="1" set __add=1
+    IF /I "%_INPUT%"=="%_TEMP%" set __add=1& set __selected=0
+    if "%__selected%"=="1" if "%__sysentry%"=="1" echo %R%cannot remove from %__type% %__VAR__%: %W%"%_TEMP%"%N%
+    if "%__add%"=="1" (
       IF defined _RESULT (
         SET "_RESULT=%_RESULT%;%_INPUT%"
       ) ELSE (
         SET "_RESULT=%_INPUT%"
       )
     ) else (
-      echo %R%%__msg% from %__src_name%: %W%"%_TEMP%"%N%
+      echo %__msg% from %__type% %__VAR__%: %W%"%_TEMP%"%N%
     )
   goto :EOF
   :write_reg_path
