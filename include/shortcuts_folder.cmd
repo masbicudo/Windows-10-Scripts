dfcaa92c67641a7bb10233a93ad906b304d7d31b
Miguel Angelo <masbicudo@gmail.com>
2020-06-21 19:36:01 -0300

    includes: shortcuts_folder.cmd - install Everything only if needed

--- include/shortcuts_folder.cmd
+++ include/shortcuts_folder.cmd
@@ -4,7 +4,7 @@ if defined where_shortcuts goto :eof
 :: if choco is not installed then install it
 where choco || call require apps\choco.cmd
 :: if everything is not installed then install it
-choco install -y everything
+where es || choco install -y everything
 
 setlocal
 call set-out.cmd where_shortcuts es.exe Shortcuts !\program !network !printer !application !amd64 !implicit !\Chrome !\AppData !*.*
